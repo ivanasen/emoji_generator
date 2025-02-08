37ecf36f81bbc0462c2c1129903fc37e4b5e2943
ivanasen <ivanasenchakarov@gmail.com>
2025-02-08 21:17:00 +0200

    finish stem model

--- stem_model.py
+++ stem_model.py
@@ -1,18 +1,31 @@
+from nltk.tokenize import WordPunctTokenizer
+from nltk.stem import PorterStemmer
+import re
 
 class StemModel(object):
+    def __init__(self, emojis_dataset, stop_words):
+        self.stop_words = stop_words
+        self.tokenizer = WordPunctTokenizer()
 
-    def __init__(self, emojis_dataset):
-        self.emojis_dataset = emojis_dataset
+        self.emojis_dataset = emojis_dataset[["Representation", "Name"]]
+        self.emojis_dataset["Stemmed"] = [self.pre_process(name) for name in emojis_dataset.Name]        
 
-    __call__ = call
+
+
+    def pre_process(self, sentence: str) -> list[str]:
+      tokens = self.tokenizer.tokenize(sentence)
+      stemmer = PorterStemmer()
+      stemmed_sent = [stemmer.stem(t) for t in tokens]
+      tokens = [t for t in stemmed_sent if t not in self.stop_words]
+      return tokens
 
     def call(self, t: str) -> str:
-        tokens = word_tokenize(sent)
+        tokens = self.tokenizer.tokenize(t)
 
         result = ""
 
         for token in tokens:
-          processed = pre_process(token)
+          processed = self.pre_process(token)
 
           if len(processed) == 0: # It is stop-word
             result += token + " "
@@ -33,57 +46,14 @@ class StemModel(object):
           if len(matches) > 0:
             # result += matches[random.randint(0, len(matches) - 1)][0]
             # result += choose_random_weighted(matches)[0]
-            result += choose_shortest_len(matches)[0]
+            result += self.choose_shortest_len(matches)[0]
           else:
             result += token
           result += " "
 
         return result
 
+    __call__ = call
 
-
-
-def pre_process(sentence: str) -> list[str]:
-  ps = PorterStemmer()
-  clean_sent = re.sub(r'[^\w\s]', '', sentence)
-  tokens = word_tokenize(clean_sent)
-  stemmed_sent = [ps.stem(t) for t in tokens]
-  tokens = [t for t in stemmed_sent if t not in stop_words]
-  return tokens
-
-def choose_shortest_len(matches: list[tuple[str, list[str]]]) -> str:
-  return sorted(matches, key=lambda x: len(x[1]))[0]
-
-def stem_model(sent: str) -> str:
-  tokens = word_tokenize(sent)
-
-  result = ""
-
-  for token in tokens:
-    processed = pre_process(token)
-
-    if len(processed) == 0: # It is stop-word
-      result += token + " "
-      continue
-
-    processed = processed[0]
-
-    matches = []
-
-    for _, emoji in emojis_dataset.iterrows():
-      #for emoji_token in emoji.Stemmed:
-      #  dist = edit_distance(processed, emoji_token)
-      #  if dist < 2:
-      #    matches.append((emoji.Representation, emoji.Stemmed))
-      if processed in emoji.Stemmed:
-        matches.append((emoji.Representation, emoji.Stemmed))
-
-    if len(matches) > 0:
-      # result += matches[random.randint(0, len(matches) - 1)][0]
-      # result += choose_random_weighted(matches)[0]
-      result += choose_shortest_len(matches)[0]
-    else:
-      result += token
-    result += " "
-
-  return result
+    def choose_shortest_len(self, matches: list[tuple[str, list[str]]]) -> str:
+      return sorted(matches, key=lambda x: len(x[1]))[0]
